87913144c80d3e53ce396e81c181936342994d11
emfo2405 <emfo2405@student.miun.se>
2026-02-16 12:24:30 +0100

    Interfaces skapade för hantering av autentisering

--- blogFrontend/src/components/navbar.tsx
+++ blogFrontend/src/components/navbar.tsx
@@ -8,6 +8,7 @@ function Navbar() {
     <header>
         <ul>
             <li><NavLink to="/">Hem</NavLink></li>
+            <li><NavLink to="/blog">Blogg</NavLink></li>
             <li><NavLink to="/admin">Admin</NavLink></li>
             <li><NavLink to="/login">Logga in</NavLink></li>
         </ul>
--- blogFrontend/src/pages/LogIn.tsx
+++ blogFrontend/src/pages/LogIn.tsx
@@ -2,10 +2,31 @@ import { useState } from 'react'
 
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError('');
+    }
 
   return (
     <>
-    <h1>Välkommen till Login!</h1>
+    <h1>Logga in med dina uppgifter!</h1>
+
+    <form onSubmit={handleSubmit}>
+        {error && (
+            <p>{error}</p>
+        )}
+        <label htmlFor='email'>E-postadress:</label><br/>
+        <input type='email' id='email' name='email' required value={email} onChange={(e) => setEmail(e.target.value)}></input><br/>
+
+        <label htmlFor='password'>Lösenord:</label><br/>
+        <input type='password' id='password' name='password' required value={password} onChange={(e) => setPassword(e.target.value)}></input><br/>
+
+        <input type='submit' value="Logga in"></input>
+    </form>
     </>
   )
 }
