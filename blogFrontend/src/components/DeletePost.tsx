60c07b62b0e9e4fabf6a4f98f25000bbb3db990f
emfo2405 <emfo2405@student.miun.se>
2026-02-18 15:06:43 +0100

    Funktion för att radera inlägg skapat

--- blogFrontend/src/components/DeletePost.tsx
+++ blogFrontend/src/components/DeletePost.tsx
@@ -1,8 +1,37 @@
 
-function DeletePost() {
+
+interface DeleteProps {
+    id:number,
+    getPosts: () => void;
+}
+
+const token = localStorage.getItem("token");
+
+function DeletePost({id, getPosts}: DeleteProps) {
+
+const deletePost = async (id:number) => {
+    try {
+        const resp = await fetch(`https://blogposts-frontendm3.onrender.com/api/post/${id}/`, {
+            method: "DELETE",
+            headers: {
+                    "Authorization": `Bearer ${token}`,
+                    "Content-Type": "application/json",
+            }
+        });
+
+        if(!resp.ok) {
+            throw new Error("Delete misslyckades");
+        } else {
+            getPosts();
+        }
+    } catch(error) {
+        throw error;
+    }
+}
 
   return (
 <>
+    <button onClick={() => deletePost(id)}>Radera inlägg</button>
 </>
   )
 }
