43bf6ed6adc162b77b3220e0372785417d7fd9b8
emfo2405 <emfo2405@student.miun.se>
2026-02-19 19:12:29 +0100

    Design och innehåll tillagt för startsidan

--- blogFrontend/src/pages/Home.tsx
+++ blogFrontend/src/pages/Home.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
 
+import './Home.scss'
 
 function Home() {
 
   return (
     <>
-    <h1>Välkommen till startsidan!</h1>
+    <h1 id="introtitle">Välkommen till Resebloggen!</h1>
+    <p id="introtext">Välkommen hit! Jag heter Emma och här 
+      kommer jag dela med mig av mina äventyr runt om i världen. Här kommer 
+      du hitta resetips, de bästa restaurangerna jag har besökt runt om i världen och 
+      några gömda pärlor som man inte får missa. <br/>
+      <br/>
+      Hoppas du kommer hitta inspiration till din kommande resa här!
+    </p>
     </>
   )
 }
