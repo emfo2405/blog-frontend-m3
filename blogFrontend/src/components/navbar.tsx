c0e624c69844b4566e9251a26f6fae938e4fc828
emfo2405 <emfo2405@student.miun.se>
2026-02-19 19:01:35 +0100

    CSS tillagd för meny och header-bild inkluderad

--- blogFrontend/src/components/navbar.tsx
+++ blogFrontend/src/components/navbar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom';
+import './Navbar.scss'
 
 
 function Navbar() {
@@ -16,15 +17,19 @@ function Navbar() {
 
   return (
     <header>
-        <ul>
-            <li><NavLink to="/">Hem</NavLink></li>
-            <li><NavLink to="/blog">Blogg</NavLink></li>
-            <li><NavLink to="/admin">Admin</NavLink></li>
-            <li>{
-              !user ? <NavLink to="/login">Logga in</NavLink> : <button onClick={handleLogout}>Logga ut</button>
+      <div className='menu'>
+                <ul>
+            <li><NavLink className={({ isActive }) => isActive ? "navLink active" : "navLink"} to="/">Hem</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? "navLink active" : "navLink"} to="/blog">Blogg</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? "navLink active" : "navLink"} to="/admin">Admin</NavLink></li>
+            <li id="logout-placement">{
+              !user ? <NavLink className={({ isActive }) => isActive ? "navLink active logout" : "navLink logout"} to="/login">Logga in</NavLink> : <button className='logout' onClick={handleLogout}>Logga ut</button>
               }
               </li>
         </ul>
+      </div>
+      <img className='headerImg' src='/blogHeader.jpg' alt='Bild av ett tåg som åker över en bro'></img>
+
     </header>
   )
 }
